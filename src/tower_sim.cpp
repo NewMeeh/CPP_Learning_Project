33774b3026a61e9b5a0c73593ebaf291fe40ad45
NewMeeh <thomas.saillard@protonmail.com>
2022-04-01 17:59:45 +0200

    Task 2 - 1 - A Done

--- src/tower_sim.cpp
+++ src/tower_sim.cpp
@@ -50,9 +50,9 @@ void TowerSimulation::display_help() const
     std::cout << "This is an airport tower simulator" << std::endl
               << "the following keysstrokes have meaning:" << std::endl;
 
-    for (const auto& ks_pair : GL::keystrokes)
+    for (const auto& [c, f] : GL::keystrokes)
     {
-        std::cout << ks_pair.first << ' ';
+        std::cout << c << ' ';
     }
 
     std::cout << std::endl;
